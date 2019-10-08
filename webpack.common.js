e52d39e95c8c41feccac8e77f8548e7f369c7543
Cormorant Programming <cormorant.programming@gmail.com>
2019-10-08 22:43:06 +0700

    html-, file-loader & clean-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,5 +1,6 @@
 const path = require("path");
 var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
   entry: "./src/index.js",  // default value is "./src/index.js"
@@ -15,12 +16,30 @@ module.exports = {
           'sass-loader'   //  1.  turns sass into css
         ],
       },
+      {
+        test: /\.html$/i,
+
+        use: ['html-loader'],
+      },
+      {
+        test: /\.(svg|png|jpg|gif)$/i,
+
+        use: {
+          loader: 'file-loader',
+          options: {
+            name: "[name].[hash].[ext]",
+            outputPath: "imgs"
+          }
+        }
+      },
     ],
   },
 
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/template.html",
-    })
+    }),
+
+    new CleanWebpackPlugin()
   ],
 }

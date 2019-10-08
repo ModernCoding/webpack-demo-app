ab5d864a639d977e210ce359347885135eaa5a5f
Cormorant Programming <cormorant.programming@gmail.com>
2019-10-08 20:10:54 +0700

    webpack.config.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,4 @@ import { run } from './app/app';
 import { AlertService } from './app/alert.service';
 import { ComponentService } from './app/component.service';
 
-const alertService = new AlertService();
-const componentService = new ComponentService();
-
-run(alertService, componentService);
+run(new AlertService(), new ComponentService());

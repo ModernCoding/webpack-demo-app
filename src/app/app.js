29e500331f8a245a215a1ef5b70e7e4f5f9c8bd6
Cormorant Programming <cormorant.programming@gmail.com>
2019-10-08 17:49:36 +0700

    webpack

--- src/app/app.js
+++ src/app/app.js
@@ -6,8 +6,10 @@ const run = (alertService, componentService) => {
 
   componentService.onClick(() => {
     alertService.hideErrors();
+
     const inputs = componentService.getInputs();
     const parsedInputs = parseInputs(...inputs);
+    
     if (inputsAreValid(...parsedInputs)) {
       const [numA, numB] = parsedInputs;
       componentService.setResult(numA + numB);
